eb6e5e8bd0b2566701b348ec9af6f4ad52fd5515
Lunysska <lunysska@gmail.com>
2022-10-31 21:03:11 -0600

    Add files via upload

---
+++ src/modulo2.R
@@ -0,0 +1,22 @@
+#Virginia Teodosio<virginia@ciencias.unam.mx>
+#28 octubre de 2022
+# sesion 1
+
+#esta función es para mostrar la ayuda en R
+help()
+
+#Se puede usar pasándole como argumento el nombre de alguna otra función
+help(rnorm)
+
+#Esta llamada es equivalente
+?rnorm
+
+#Es la llamada de inicio de la ayuda de R
+help.start()
+
+#Para saber el directorio de trabajo actual
+getwd()
+
+#esta linea es modificar el directorio de trabajo actual
+#en windows
+setwd("C:/Users/UsuariO/Desktop/cursoRParaLasCienciasSociales/sesion1")
