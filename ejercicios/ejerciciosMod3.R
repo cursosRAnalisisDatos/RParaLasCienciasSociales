593688dbad8e2f8effe5dc259eb770137556923c
Lunysska <lunysska@gmail.com>
2022-12-08 12:56:31 -0600

    Add files via upload

---
+++ ejercicios/ejerciciosMod3.R
@@ -0,0 +1,77 @@
+# @author: Virginia Teodosio
+# @email: lunysska@gmail.com
+# @date: Nov, 2022.
+# @version: 1.0
+
+# Objetivo: Que el participante se familiarice con errores en el código (en 
+# la terminal de R y de tipo semántico) y pueda identificarlos y corregirlos.
+
+# Instrucciones: 
+# 1. Ejecuta una línea a la vez, en el orden de aparición,
+# 2. Corrígelos en ese mismo orden 
+# 3. Ve incluyendo en el script los errores que van generando, en líneas comentadas
+
+# ------------------------------------------------------------------------------
+# Ejercicios con matrices
+# 1. (RESPUESTA:índice fuera de los límites)
+matriz <- matrix(1:12, nrow = 4, ncol = 3)
+matriz
+x <- matriz[1, 4]
+
+# 2. Extrayendo una columna de la matriz (RESPUESTA: error semántico, porque se esta extrayendo el renglón)
+columna <- matriz[4,]
+columna
+
+# ------------------------------------------------------------------------------
+# Ejercicios con listas
+# 1. Definiendo una lista
+#Dada la lista
+lst <- list(name="Fred", wife="Mary", no.children=3, child.ages=c(4,7,9))
+
+#queremos saber que tamaño tiene la lista 
+#(RESPUESTA: error, la funcion no es size, es length!)
+size(lst)
+
+# 2. Queremos conocer el elemento en la posicion 3, se debe usar la sintáxis adecuada
+lst[[3]]
+lst[3]
+# 2.1 Cual es la diferencia de ambas expresiones? (RESPUESTA la primera es el elemento 
+#numérico, la segunda es el componente, por lo tanto es una lista)
+
+# 2.2 Intente operar aritméticamente con ambas expresiones
+# RESPUESTA (eliminar las siguientes dos líneas)
+# lst[3] + 2 
+# lst[[3]] + 2
+
+# 2.3 Con qué función podríamos conocer de qué tipo es el dato(RESPUESTA, con la función 
+#class, uno dirá que es numérico y el otro lista)
+#Respuesta: (eliminar las siguientes dos líneas)
+#class(lst[[3]])
+#class(lst[3])
+
+# 2.4 Qué pasa si se crea una lista D, de la unión de tres listas A, B, C, donde cada 
+# una de ellas tienen una variable con el mismo nombre "name" y se imprime D$name
+# qué valor exhibe?
+# Pruébelo!
+# RESPUESTA la primer ocurrencia que encuentre
+# RESpuesta, código que se debe borrar, las siguientes 5 lineas
+#lst.A <- list(name="uno", wife="Mary", noChildren=3, childAges=c(4,7,9) )
+#lst.B <- list(name="dos", wife="Mary", noChildren=3, childAges=c(4,7,9) )
+#lst.C <- list(name="tres", wife="Mary", noChildren=3, childAges=c(4,7,9) )
+#lst.ABC <- c(lst.A, lst.B, lst.C )
+#lst.ABC$name
+
+# ------------------------------------------------------------------------------
+# Ejercicios con dataframes
+
+#1. Dados los siguientes datos, genere un dataframe
+nombre <- c("Juan", "Margarita", "Ruben", "Daniel")
+apellido <- c("Sanchez", "Garcia", "Sancho", "Alfara")
+fecha_nacimiento <- c("1976-06-14", "1974-05-07", "1958-12-25", "1983-09-19")
+sexo <- c("HOMBRE", "MUJER", "HOMBRE", "HOMBRE")
+nro_hijos <- c(1, 2, 1, 3, 2)
+
+#Acá va la linea del dataframe
+#RESPUESTA
+censo <- data.frame(nombre, apellido, fecha_nacimiento, sexo, nro_hijos)
+#pero va a generar un error, porque la longitud del vector nro_hijos es más grande
