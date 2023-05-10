e4bf89549b646c52ec2ca2d2bc38f71a30d23d2c
Lunysska <lunysska@gmail.com>
2023-05-09 18:14:14 -0600

    Update respuestasMod3_Act1.R

--- tareas/_extras/modulo3/respuestasMod3_Act1.R
+++ tareas/_extras/modulo3/respuestasMod3_Act1.R
@@ -17,16 +17,3 @@
 # ¿Qué preguntas consideras que sería relevante plantear?
 #   
 #   Nota: los nombres son ficticios.
-# Actividad 2. Correspondiente a temas de: dataframes
-# 
-# Transformar la actividad de correspondientes a "vectores, listas y matrices" en 
-#un dataframe, y a partir de ese dataframe:
-#   
-# Cómo obtener la edad de Felipe
-# Esta respuesta tiene que ver con agregar valores NA en campos no dados, para 
-#controlar los datos.
-
-# Cómo obtener el pronóstico de Leticia y Rosa
-# Agregar una columna que tome en cuenta el peso de los pacientes; los datos recabados son: Juan Pérez, 85 kg; Leticia Ramírez, 68 kg; Rosa Flores, 59 kg; Josue Navarrete, 96 kg; Felipe García, 73 kg
-# Reordena tu data.frame por edades; reordena por tipo de enfermedad
-# ¿Se puede hallar la edad promedio de los pacientes? Si es así, cuál es.
