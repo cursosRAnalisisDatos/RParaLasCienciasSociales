af197617c3e424e6ffd02fbbaeb3184bd731a6ce
Lunysska <lunysska@gmail.com>
2024-06-15 15:29:01 -0600

    Update ejerciciosMod7.R

--- ejercicios/ejerciciosMod7.R
+++ ejercicios/ejerciciosMod7.R
@@ -124,4 +124,95 @@ ggplot(data = notas) +
   geom_point(aes(x = nota09, y = nota10, shape= tipo))   # una forma para cada 
                                                         # tipo de colegio
 
-#
+#Transformaciones estadísticas
+#Algunos geoms requieren llevar a cabo alguna transformación estadística 
+#(técnicamente, un stat) que toma un conjunto de datos y crea otro nuevo. De 
+#esta forma combinando geom y stat se pueden obtener una gran variedad de 
+#visualizaciones. A veces se puede asignar una transformación estadística a un 
+#geom mediante el argumento method. Por ejemplo, geom_smooth usa distintos 
+#métodos para estimar la media condicionada (curva de regresión) de una variable 
+#por otra, incluyendo el método de mínimos cuadrados, que es el más conocido:
+
+ggplot(data = notas) +
+  geom_point(aes(x = nota09, y = nota10),
+             col = 'darkblue',
+             size = 3,
+             alpha = 1/5) + 
+  geom_smooth(aes(x = nota09, y = nota10), method = 'lm')    # 
+
+# La opción por defecto no es la recta de mínimos cuadrados, sino un método de 
+# suavizado no paramétrico:
+ggplot(data = notas) +
+  geom_point(aes(x = nota09, y = nota10),
+             col = 'darkblue',
+             size = 3,
+             alpha = 1/5) + 
+  geom_smooth(aes(x = nota09, y = nota10))    # Opción por defecto
+
+#Diagramas de barras
+#Se usa geom_bar. La operación estadística por defecto (el statque se usa 
+#                                                       implícitamente) es 
+#calcular las frecuencias de cada uno de los valores de la variable que se asigna 
+#a x. En comparación con los ejemplos anteriores, basta especificar la coordenada 
+#x en lugar de la xy la y, ya que la variable asignada a y por defecto son las 
+#frecuencias.
+#Veamos dos ejemplos, el primero con todas las opciones por defecto y el segundo
+#con el aspecto modificado.
+ggplot(data = notas) +
+  geom_bar(aes(x = tipo))
+
+# Algunos parámetros modificados
+ggplot(data = notas) +
+  geom_bar(aes(x = tipo),
+           width=0.3,
+           fill='tomato2') +
+  theme_classic()
+
+#Si disponemos ya del vector de frecuencias, hay que cambiar varios detalles del 
+#código anterior. Supongamos que hay tres grupos A, B y C con frecuencias 20, 20 
+#y 60 respectivamente. En este caso hay que asignar la variable del eje x
+#y también la del eje y. Además, hay que cambiar el stat por defecto (recordamos 
+#que este es contar el número de observaciones de cada valor). El nuevo states 
+#identity que consiste en dejar la variable asignada a y (es decir, las 
+#frecuencias) tal y como está sin modificar nada.
+datos_frecuencias <- data.frame(grupo = c('A','B','C'), frecuencias = c(20, 20, 60))
+ggplot(data = datos_frecuencias) + 
+  geom_bar(aes(x = grupo, y = frecuencias),
+           stat = 'identity',
+           width = 0.3,
+           fill='tomato2')  +
+  theme_classic()# el argumento stat modifica la opción por defecto
+
+#Histogramas
+#Para representar un histograma se usa geom_histogram. Con las opciones por defecto resulta:
+  ggplot(data = notas) +
+  geom_histogram(aes(x = nota09))
+
+  #Con el siguiente código cambiamos el número de rectángulos del histograma, los 
+  #colores y otros aspectos del gráfico:
+  ggplot(notas) +
+    geom_histogram(aes(x = nota09,
+                       y=..density..),  # para que el área sea 1
+                   bins=10,
+                   fill='lightblue',
+                   col='black') +
+    labs(x = 'Notas de 2009', y = 'Frecuencias', title = 'Histograma') +
+    facet_wrap( ~ tipo, nrow = 3) +
+    theme_minimal() +  
+    theme(panel.grid = element_blank())  # elimina grid de fondo
+
+#Diagramas de cajas
+#Para representar diagramas de cajas se usa geom_boxplot.
+  ggplot(notas) +
+    geom_boxplot(aes(x = tipo, y = nota09 ))
+
+#Para cambiar los ejes se usa coord_flip. Es posible añadir los puntos con 
+#geom_jitter: 
+  ggplot(notas) +
+    geom_boxplot(aes(x = tipo, y = nota09)) + 
+    coord_flip() +
+    geom_jitter(aes(x = tipo, y = nota09), 
+                size = 2,
+                alpha = 0.1,
+                width = 0.1)
+  
