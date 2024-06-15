27a4f1e36a65dfd20a189a6655db82bd73d7b2d4
Lunysska <lunysska@gmail.com>
2024-06-15 14:47:39 -0600

    Update ejerciciosMod7.R

--- ejercicios/ejerciciosMod7.R
+++ ejercicios/ejerciciosMod7.R
@@ -54,3 +54,74 @@ df <- data.frame(datosInegiConMunicipiosOrdenado,
 ggplot(df, aes(x = datosInegiConMunicipiosOrdenado$EDAD, 
                fill = datosInegiConMunicipiosOrdenado$SEXO_CHAR)) + 
   geom_histogram()
+
+############################################################################
+### Ejercicio 2.
+#Traducción del inglés-Tidyverse es una colección de paquetes de código abierto 
+#para el lenguaje de programación R presentado por Hadley Wickham y su equipo que
+#"comparten una filosofía de diseño, gramática y estructuras de datos subyacentes" 
+#de datos ordenados.
+#install.packages("tidyverse")
+library(tidyverse)
+#install.packages("ggplot2")
+library(ggplot2)
+
+# Ejemplo perfecto donde se lee el archivo desde un servidor
+# Ver: 'http://verso.mat.uam.es/~joser.berrendero/datos/notas.txt'
+#Son datos separados por coma, por eso se usa otra función, read.table()
+
+#Utilizamos el fichero notas que contiene las notas medias en 1000 colegios de 
+#una prueba de nivel que realizó la Comunidad de Madrid en 2009 y 2010, junto 
+#con el tipo de colegio (concertado, privado o público). Lo primero, cargamos 
+#ggplot2 y leemos los datos:
+  
+notas <- read.table('http://verso.mat.uam.es/~joser.berrendero/datos/notas.txt',
+                    sep = ' ',
+                    dec = ',',
+                    header=TRUE)
+head(notas)
+
+#i) Crear un gráfico y añadir capas
+#Para crear un gráfico se usa el comando ggplot:
+ggplot(data = notas)
+
+#De momento solo hemos asignado al gráfico el conjunto de datos que queremos 
+#visualizar. No se representa gráficamente nada hasta que no se añaden más capas. 
+#Importante: los datos a representar siempre tienen que formar parte de un data 
+#frame.
+
+#Las capas sirven para proporcionar información sobre cómo queremos visualizar 
+#los datos. Esto se lleva a cabo a través de un geom. Este es un ejemplo sencillo 
+#usando geom_point que es el geomcorrespondiente un diagrama de dispersión (cada 
+#tipo de gráfico tiene el suyo). En primer lugar, mediante aes asignamos las 
+#coordenadas x e y a los valores de las notas de 2009 y 2010 respectivamente.
+
+ggplot(data = notas) +
+  geom_point(aes(x = nota09, y = nota10))
+
+#Si queremos añadir información sobre el tipo de colegio, lo podemos hacer 
+#especificando que el color con el que se representa el punto dependa de la 
+#variable "tipo":
+ggplot(data = notas) +
+  geom_point(aes(x = nota09, y = nota10, col = tipo))
+
+#Vemos como el argumento aes (que corresponde a aesthetics) asigna variables a 
+#ciertos aspectos del gráfico. Estos aspectos se pueden fijar en lugar de hacerlos 
+#depender de una variable. En este caso el valor del argumento se asigna fuera de 
+#la lista de aesthetics. En el siguiente ejemplo se ha fijado el color de los 
+#puntos, su tamaño y el grado de transparencia (lo que es útil cuando hay muchos 
+#                                               puntos):
+ggplot(data = notas) +
+  geom_point(aes(x = nota09, y = nota10),
+             col = 'darkblue',    # color de los puntos
+             size = 3,            # tamaño de los puntos
+             alpha = 1/5)   
+
+#En general, cada geom admitirá un conjunto determinado de aesthetics. En el caso 
+#particular de geom_point, mediante aes podemos hacer depender de una variable 
+#otros aspectos como la forma o el tamaño de los puntos:
+ggplot(data = notas) +
+  geom_point(aes(x = nota09, y = nota10, shape= tipo))   # una forma para cada 
+                                                        # tipo de colegio
+
+#
